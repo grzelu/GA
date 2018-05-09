3659653072ea3c7a61baf5b4832d96e132da17a2
grzelu <grzejszczakmichal9@gmail.com>
2018-05-09 10:42:07 +0200

    ll

--- new.py
+++ new.py
@@ -12,11 +12,36 @@ mutation_probability = 0.08
 
 
 #Generate random population
-populacja = population(10,8,generate=False)
+populacja = population(10,7,generate=False)
 populacja.calculateFitness()
 #populacja_bkp = populacja
+fit = 0
 for i in populacja.population_list:
-    print (i.gene_list,i.fitness)
+    print (i.gene_list,i.fitness,i.n_fitness)
+    fit +=i.n_fitness
+print(fit)
+
+def alfaSelect (population,precent):
+    pop = copy.deepcopy(population)
+    pop.population_list.sort(key=lambda x:x.n_fitness, reverse = False)
+    next_gen= []
+    for i in pop.population_list[:round(len(pop.population_list)*precent)]:
+        print (i.gene_list,i.n_fitness,i.fitness)
+        next_gen.append(i)
+    return next_gen
+ng = alfaSelect(populacja,0.5)
+print('before')
+for i in ng:
+    print(i.gene_list)
+for i in populacja.population_list:
+    i.gene_list[1]=list_of_cities[0]
+print('after')
+for i in ng:
+    print(i.gene_list)
+print('initial population')
+for i in populacja.population_list:
+    print (i.gene_list,i.fitness,i.n_fitness)
+ 
 best = 0
 best_chr=[]
 #for i in range(0,100):
